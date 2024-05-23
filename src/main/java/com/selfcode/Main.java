458040f38ad610cf249f0bc30d38c094a6813f28
Oleksii Rozumenko <60263239+tasukeua@users.noreply.github.com>
2024-05-23 19:05:01 +0300

    Update Main.java

--- src/main/java/com/selfcode/Main.java
+++ src/main/java/com/selfcode/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<LocalDate> dataCollection = new ArrayList<LocalDate>();
+        List<LocalDate> dataCollection = new ArrayList<>();
         dataCollection.add(LocalDate.of(2004, 7, 1));
         dataCollection.add(LocalDate.of(2005, 1, 2));
         dataCollection.add(LocalDate.of(2007, 1, 1));
